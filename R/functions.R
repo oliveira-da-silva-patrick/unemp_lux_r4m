e27597409d0992dd8339ba65f040d64071e3dcd7
Bruno Rodrigues <bruno@brodrigues.co>
2022-10-18 15:25:43 +0200

    clean_unemp

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,16 @@
 my_function <- function(a, b){
     a + b
 }
+
+my_filter <- function(dataset, condition){
+    dataset |>
+        dplyr::filter({{condition}})
+}
+
+clean_unemp <- function(unemp_data, level, col_of_interest){
+
+  unemp_data |>
+    janitor::clean_names() |>
+    dplyr::filter({{level}}) |>
+    dplyr::select(year, commune, {{col_of_interest}})
+}
