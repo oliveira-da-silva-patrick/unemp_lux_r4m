c517af137a44336b1c6e453919af029a3b0f973f
Bruno Rodrigues <bruno@brodrigues.co>
2022-10-18 16:48:37 +0200

    namespace and description

--- R/functions.R
+++ R/functions.R
@@ -1,12 +1,26 @@
-my_function <- function(a, b){
-    a + b
-}
-
-my_filter <- function(dataset, condition){
-    dataset |>
-        dplyr::filter({{condition}})
-}
-
+#' Cleans unemployment data for Luxembourg
+#' @param unemp_data A data frame containing unemployment data for Luxembourg.
+#' @param level A predicate condition indicating the regional level of interest. See details for more.
+#' @param col_of_interest A column of the `unemp_data` data frame that you wish to select.
+#' @importFrom janitor clean_names
+#' @importFrom dplyr filter select
+#' @return A data frame
+#' @export
+#' @details
+#' This function allows the user to create a data frame for several regional levels. The first level
+#' is the complete country. The second level are cantons, and the third level are neither cantons
+#' nor the whole country, so the communes. Individual communes can be selected as well.
+#' `level` must be predicate condition passed down to dplyr::filter. See the examples below
+#' for its usage.
+#' @examples
+#' # Filter on cantons
+#' clean_unemp(unemp_2013,
+#'             grepl("Canton", commune),
+#'             active_population)
+#' # Filter on a specific commune
+#' clean_unemp(unemp_2013,
+#'             grepl("Kayl", commune),
+#'             active_population)
 clean_unemp <- function(unemp_data, level, col_of_interest){
 
   unemp_data |>
